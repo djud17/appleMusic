f81684e8d4c427f5b5e78957723792a7d4b1d502
David Tonoyan <davidtonoyan10@me.com>
2023-03-29 12:02:00 +0400

    Project upload

---
+++ appleMusic/appleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  appleMusicApp.swift
+//  appleMusic
+//
+//  Created by Давид Тоноян  on 29.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct appleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
